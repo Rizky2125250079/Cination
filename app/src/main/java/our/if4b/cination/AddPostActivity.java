19a5b7626a26e44bbefe8477d4545f4a189e334d
Veldy P <veldy.piscessara@gmail.com>
2023-06-08 00:36:00 +0700

    Update AddPostActivity.java

--- app/src/main/java/our/if4b/cination/AddPostActivity.java
+++ app/src/main/java/our/if4b/cination/AddPostActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
 
 import our.if4b.cination.databinding.ActivityAddPostBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -45,6 +51,34 @@ public class AddPostActivity extends AppCompatActivity {
     }
 
     private void addPost(String username, String negara, String kota) {
-        
+        binding.progressBar.setVisibility(View.VISIBLE);
+        ApiService api = Utility.getRetrofit().create(ApiService.class);
+        Call<ValueData<List<Post>>> call = api.getPost();
+        call.enqueue(new Callback<ValueData<List<Post>>>() {
+            @Override
+            public void onResponse(Call<ValueData<List<Post>>> call, Response<ValueData<List<Post>>> response) {
+                if(response.code() == 200){
+                    int success = response.body().getSuccess();
+                    String massage = response.body().getMessage();
+                    if(success == 1){
+                        Toast.makeText(AddPostActivity.this, massage, Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueData<List<Post>>> call, Throwable t) {
+                binding.progressBar.setVisibility(View.GONE);
+                System.out.println("Retrofit Error : " + t.getMessage());
+                Toast.makeText(AddPostActivity.this, "Retrofit Error : " + t.getMessage()  , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
