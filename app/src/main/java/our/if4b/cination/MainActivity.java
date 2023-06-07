f7980e89db11c4d5dfea5958f1ed4bb10e68b4bc
riski <113693371+Rizky2125250079@users.noreply.github.com>
2023-06-07 16:00:03 +0700

    Initial commit

---
+++ app/src/main/java/our/if4b/cination/MainActivity.java
@@ -0,0 +1,14 @@
+package our.if4b.cination;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
