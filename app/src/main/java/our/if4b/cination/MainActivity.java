e3f1fc2cc2fe84da1dba452e4cafcade0eae8b3d
FhLevi13 <alifvahlephi46@gmail.com>
2023-06-08 00:08:20 +0700

    Update MainActivity.java

--- app/src/main/java/our/if4b/cination/MainActivity.java
+++ app/src/main/java/our/if4b/cination/MainActivity.java
@@ -7,11 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
 import our.if4b.cination.databinding.ActivityMainBinding;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +40,32 @@ public class MainActivity extends AppCompatActivity {
     private void getAllPost() {
         ApiService api = Utility.getRetrofit().create(ApiService.class);
         Call<ValueData<List<Post>>> call = api.getPost();
+
+        call.enqueue(new Callback<ValueData<List<Post>>>() {
+            @Override
+            public void onResponse(Call<ValueData<List<Post>>> call, Response<ValueData<List<Post>>> response) {
+                if (response.code() == 200) {
+                    int success = response.body().getSuccess();
+                    String message = response.body().getMessage();
+
+                    if (success == 1) {
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                        data = response.body().getData();
+                        postViewAdapter.setData(data);
+                    } else {
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueData<List<Post>>> call, Throwable t) {
+                System.out.println("Retrofit Error : " + t.getMessage());
+                Toast.makeText(MainActivity.this, "Retrofit Error : " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
