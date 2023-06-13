2aa53e69a87e554ebf49abc13be16068e043a9eb
Veldy P <veldy.piscessara@gmail.com>
2023-06-14 01:16:35 +0700

    Up

--- app/src/main/java/our/if4b/cination/AddPostActivity.java
+++ app/src/main/java/our/if4b/cination/AddPostActivity.java
@@ -7,15 +7,12 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
-import java.util.List;
-
 import our.if4b.cination.databinding.ActivityAddPostBinding;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AddPostActivity extends AppCompatActivity {
-
     private ActivityAddPostBinding binding;
 
     @Override
@@ -28,8 +25,8 @@ public class AddPostActivity extends AppCompatActivity {
         binding.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String negara = binding.etNegara.getText().toString();
                 String ibukota = binding.etKota.getText().toString();
+                String negara = binding.etNegara.getText().toString();
 
                 boolean bolehPost = true;
 
@@ -39,45 +36,47 @@ public class AddPostActivity extends AppCompatActivity {
                 }
                 if(TextUtils.isEmpty(ibukota)){
                     bolehPost = false;
-                    binding.etKota.setError("Ibu Kota tidak boleh kosong!");
+                    binding.etKota.setError("Kota tidak boleh kosong!");
                 }
                 if(bolehPost){
-                    String user_id = Utility.getValue(AddPostActivity.this,"xUsername");
-                    addPost(user_id, negara, ibukota);
-                }
-                
-            }
-        });
-    }
+                    String userId = Utility.getValue(AddPostActivity.this,"xUserId");
+                    addPost(userId, ibukota, negara);
 
-    private void addPost(String user_id, String negara, String ibukota) {
-        binding.progressBar.setVisibility(View.VISIBLE);
-        ApiService api = Utility.getRetrofit().create(ApiService.class);
-        Call<ValueNoData> call = api.addPost(user_id,negara,ibukota);
-        call.enqueue(new Callback<ValueNoData>() {
-            @Override
-            public void onResponse(Call<ValueNoData> call, Response<ValueNoData> response) {
-                if(response.code() == 200){
-                    int success = response.body().getSuccess();
-                    String massage = response.body().getMessage();
-                    if(success == 1){
-                        Toast.makeText(AddPostActivity.this, massage, Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                    else {
-                        Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
-                    }
                 }
-                else{
-                    Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
-                }
+
             }
 
-            @Override
-            public void onFailure(Call<ValueNoData> call, Throwable t) {
-                binding.progressBar.setVisibility(View.GONE);
-                System.out.println("Retrofit Error : " + t.getMessage());
-                Toast.makeText(AddPostActivity.this, "Retrofit Error : " + t.getMessage()  , Toast.LENGTH_SHORT).show();
+            private void addPost(String userId, String ibukota, String negara) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+                ApiService api = Utility.getRetrofit().create(ApiService.class);
+                Call<ValueNoData> call =  api.addPost(userId, negara, ibukota);
+                call.enqueue(new Callback<ValueNoData>() {
+                    @Override
+                    public void onResponse(Call<ValueNoData> call, Response<ValueNoData> response) {
+                        if(response.code() == 200){
+                            int success = response.body().getSuccess();
+                            String massage = response.body().getMessage();
+                            if(success == 1){
+                                Toast.makeText(AddPostActivity.this, massage, Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(AddPostActivity.this, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ValueNoData> call, Throwable t) {
+                        binding.progressBar.setVisibility(View.GONE);
+                        System.out.println("Retrofit Error : " + t.getMessage());
+                        Toast.makeText(AddPostActivity.this, "Retrofit Error : " + t.getMessage()  , Toast.LENGTH_SHORT).show();
+
+                    }
+                });
             }
         });
     }
